8b8f5e998e00a4974dffd4cbff8395cba27d3e31
Austin Hartzheim <austin@austinhartzheim.me>
2015-07-09 05:11:52 +0000

    Added verification for UPC-A codes.

--- rigidity/rules.py
+++ rigidity/rules.py
@@ -58,3 +58,38 @@ class Strip(Rule):
 
     def apply(self, value):
         return value.strip()
+
+
+class UpcA(Rule):
+    '''
+    Validate UPC-A barscode numbers to ensure that they are 12 digits,
+    '''
+
+    def __init__(self, strict=False):
+        self.strict = strict
+
+    def apply(self, value):
+        '''
+        Cast the value to a string, then check that it is numeric.
+        Afterwards, zero-pad the left side to reach the standard length
+        of 12 digits.
+        '''
+        value = str(value)
+        if not value.isdigit():
+            raise ValueError('UPC-A code is not numeric.')
+
+        # Some barcodes become truncated by spreadsheet software that
+        #   treats the column numericly rather than as a string.
+        value = '0' * (12 - len(value)) + value
+        if len(value) > 12:
+            raise ValueError('UPC-A is longer than 12 digits')
+
+        # Verify the UPC check digit
+        if self.strict:
+            odd = sum([int(x) for x in value[0:11:2]]) * 3
+            even = sum([int(x) for x in value[1:11:2]])
+            check = (-1 * (odd + even) % 10)
+            if int(value[-1]) != check:
+                raise ValueError('UPC-A check digit is incorrect')
+
+        return value
