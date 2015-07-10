c37ea1d1d1cffc5af5840231f47501fdad4423df
Austin Hartzheim <austin@austinhartzheim.me>
2015-07-10 05:05:12 +0000

    Added setup.py for easier distribution.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Rigidity',
+    author='Austin Hartzheim',
+    author_email='austin@austinhartzheim.me',
+
+    version='0.8.1',
+    packages=['rigidity'],
+    license='GNU GPL v3',
+    description='Data-validating CSV wrapper.',
+)
