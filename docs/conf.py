08739881ab6b0366be893c2c0373aafcba124abf
Austin Hartzheim <austin@austinhartzheim.me>
2015-08-08 07:47:48 +0000

    Switched documentation to the "Read the Docs" theme.

--- docs/conf.py
+++ docs/conf.py
@@ -17,6 +17,11 @@ import sys
 import os
 import shlex
 
+try:
+    import sphinx_rtd_theme
+except ImportError:
+    pass  # Yeah, this fails on RTD...
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
@@ -115,7 +120,7 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -123,7 +128,10 @@ html_theme = 'alabaster'
 #html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+try:
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+except NameError:
+    html_theme_path = []  # RTD doesn't need this.            
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
