275b9cb0ff7348fbd8f5e25d7db01f872aee4e0a
Austin Hartzheim <austin@austinhartzheim.me>
2016-02-26 21:23:15 +0000

    v1.4.1 - Fixed two bugs impacting the use of error display settings.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     author='Austin Hartzheim',
     author_email='austin@austinhartzheim.me',
 
-    version='1.4.0',
+    version='1.4.1',
     packages=['rigidity'],
     license='GNU GPL v3',
     description='Data-validating CSV wrapper.',
